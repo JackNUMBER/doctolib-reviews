34c27c7b438a4e6cea3db249d1ea3eeabf8c0a45
JackNUMBER <antoine.cdrt@gmail.com>
2018-10-23 02:26:59 +0200

    add rating template to page

--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -1,31 +1,42 @@
-let map = new google.maps.Map(document.getElementById('map'));
-let mapService = new google.maps.places.PlacesService(map);
+const map = new google.maps.Map(document.getElementById('map'));
+const mapService = new google.maps.places.PlacesService(map);
 
 const init = () => {
-    console.log(getSearchElements());
+    getSearchElements();
 }
 
+// append rating
+const appendRating = (elm) => {
+    let rating = document.createElement('div');
+        rating.classList = 'rating';
+        rating.textContent = 'rating';
+        rating.insertAdjacentHTML( 'beforeend', '<span id="rating-stars"><span style="width: 10px"></span></span>');
+    elm.appendChild(rating);
+}
+
+// browse search DOM
 const getSearchElements = () => {
-    console.log();
     let items = document.querySelectorAll('.dl-search-result');
 
     items.forEach( function(element, index) {
-        let name = element.children[0].children[0].children[1].children[0].textContent;
+        let name = element.querySelectorAll('.dl-search-result-name')[0].textContent;
         console.log(name);
-        getPlaceInfo(name)
+        getPlaceInfo(name);
+        appendRating(element.querySelectorAll('.dl-search-result-title')[0]); // .dl-search-result-title
     });
 
     return items;
 }
 
-function getPlaceInfo(query) {
+// request place data
+const getPlaceInfo = (query) => {
   let request = {
     query: query,
     // https://developers.google.com/places/web-service/details#fields
     fields: ['name', 'formatted_address', 'rating', 'opening_hours'],
   };
 
-  mapService.findPlaceFromQuery(request, callback);
+  // mapService.findPlaceFromQuery(request, callback);
 }
 
 function callback(results, status) {
@@ -33,5 +44,3 @@ function callback(results, status) {
 }
 
 init();
-
-
