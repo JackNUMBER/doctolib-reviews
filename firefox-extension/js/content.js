8de33783b55fd53d26a4a65693f407eaa2d99e87
JackNUMBER <antoine.cdrt@gmail.com>
2018-12-01 00:52:12 +0100

    firefox version

---
+++ firefox-extension/js/content.js
@@ -0,0 +1,13 @@
+let script = document.createElement('script');
+script.src = chrome.extension.getURL('script.js');
+
+// wait window.google
+// LMK if you have any suggestion about how to improve that shit
+setTimeout(() => {
+    (document.body || document.head || document.documentElement).appendChild(script);
+}, 300)
+
+// append hidden map container
+let mapElement = document.createElement('div');
+    mapElement.id = 'map';
+document.body.appendChild(mapElement);
